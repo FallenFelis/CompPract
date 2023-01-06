9595512d509b88602df77f217a1ca9e11ab6c23e
John Doe <89201195@student.upr.si>
2023-01-07 00:40:12 +0100

    211

---
+++ task2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>//standard I/O header file
+#include<string.h>//a header file of the C standard library containing functions for working with strings ending in 0 and various memory functions.
+#include<time.h>//to work with date and time
+#include<conio.h>//to work with the console
+#include<stdlib.h>//standard library
+
