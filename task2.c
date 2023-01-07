08dbfddbe0e3a86819513399701d006b98fd8f24
John Doe <89201195@student.upr.si>
2023-01-07 02:05:27 +0100

    214

--- task2.c
+++ task2.c
@@ -5,25 +5,114 @@
 #include<stdlib.h>//standard library
 
 
+void pic(int nMaxTries) // could have been done better
+{
+    switch (nMaxTries)
+    {
+        case 0:
+
+        break;
+        case 1:
+            printf("     ______     \n");
+            printf("     |    ||    \n");
+            printf("     |    ||    \n");
+            printf("     O    ||    \n");
+            printf("          ||    \n");
+            printf("          ||    \n");
+            printf("          ||    \n");
+            printf("----------------\n");
+            printf("----------------\n");
+            break;
+        case 2:
+            printf("     ______     \n");
+            printf("     |    ||    \n");
+            printf("     |    ||    \n");
+            printf("     O    ||    \n");
+            printf("    /     ||    \n");
+            printf("          ||    \n");
+            printf("          ||    \n");
+            printf("----------------\n");
+            printf("----------------\n");
+            break;
+        case 3:
+            printf("     ______     \n");
+            printf("     |    ||    \n");
+            printf("     |    ||    \n");
+            printf("     O    ||    \n");
+            printf("    / \\  ||    \n");
+            printf("          ||    \n");
+            printf("          ||    \n");
+            printf("----------------\n");
+            printf("----------------\n");
+            break;
+        case 4:
+            printf("     ______     \n");
+            printf("     |    ||    \n");
+            printf("     |    ||    \n");
+            printf("     O    ||    \n");
+            printf("    /|\\  ||    \n");
+            printf("          ||    \n");
+            printf("          ||    \n");
+            printf("----------------\n");
+            printf("----------------\n");
+            break;
+        case 5:
+            printf("     ______     \n");
+            printf("     |    ||    \n");
+            printf("     |    ||    \n");
+            printf("     O    ||    \n");
+            printf("    /|\\  ||    \n");
+            printf("    /     ||    \n");
+            printf("          ||    \n");
+            printf("----------------\n");
+            printf("----------------\n");
+            printf("----------------\n");
+            break;
+        case 6:
+            printf("     ______     \n");
+            printf("     |    ||    \n");
+            printf("     |    ||    \n");
+            printf("     O    ||    \n");
+            printf("    /|\\  ||    \n");
+            printf("    / \\  ||    \n");
+            printf("          ||    \n");
+            printf("----------------\n");
+            printf("----------------\n");
+            break;
+        default:
+            printf("bye! T_T\n");
+            break;
+    }
+}
+
+
+
+
 int main (){
     const char *words[7] = {"artur","beautiful","cringe","boss","david","perfect","joker"}; // pack of words
     srand(time (NULL));//to get a new set of numbers every time you start
     int wordInd = rand() % 7;
     int wordLen = strlen(words[wordInd]);
     char guess[wordLen + 1];
+    int nMaxTries;
+    int i;
 
+    printf("***Hangman Game***\n");   //just a little design
+    printf("     ______     \n");
+    printf("     |    ||    \n");
+    printf("     |    ||    \n");
+    printf("     O    ||    \n");
+    printf("          ||    \n");
+    printf("          ||    \n");
+    printf("----------------\n");
+    printf("----------------\n");
 
-    printf("\n***Hangman Game***");   //just a little design
-    printf("\n     ______     ");
-    printf("\n     |    ||    ");
-    printf("\n     |    ||    ");
-    printf("\n     O    ||    ");
-    printf("\n          ||    ");
-    printf("\n          ||    ");
-    printf("\n----------------");
-    printf("\n----------------");
-
-
+    for (i = 0; i < wordLen; i++) {
+        guess[i] = '_';
+    }
+    while(nMaxTries < 6){
+        
+    }
 
 
 }
